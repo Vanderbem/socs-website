44ec96fa0d8bee680ff85292284ad8d98c7d73a6
Maggie Vanderberg <vanderbem@sou.edu>
2026-07-24 15:07:42 -0700

    remove clerk proxy matchers to ertore CDN

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/__clerk(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
@@ -19,7 +18,5 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
   ],
 }
